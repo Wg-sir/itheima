f6858bbd2e56819656b32a66c629e5e4506790dd
高建超 <gjc.com>
2022-03-07 09:36:19 +0800

    初始化vant完成

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 加载全局样式
+import './styles/index.less'
 Vue.config.productionTip = false
-
+// 注册使用Vant组件库
+Vue.use(Vant)
 new Vue({
   router,
   store,
